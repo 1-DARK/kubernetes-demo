09db797f9c025586ccaca72da20f5e917eeb1725
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-08-17 21:36:12 +0530

    edit shell script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 set -e
 
-NAME="kubernetes-demo-api"
+IMAGE_NAME="kubernetes-demo-api"
+SERVICE_NAME="devops-kubernetes-api-service"
+DEPLOYMENT_NAME="kubernetes-demo-api"
 USERNAME="1dark"
-IMAGE="$USERNAME/$NAME:latest"
+IMAGE="$USERNAME/$IMAGE_NAME:latest"
 
 echo "Building Docker Image..."
 docker build -t $IMAGE .
@@ -13,11 +15,20 @@ docker push $IMAGE
 echo "Applying Kubernetes Deployment..."
 kubectl apply -f k8s/deployment.yaml
 
+echo "Restarting rollout to pick up new image..."
+kubectl rollout restart deployment/$DEPLOYMENT_NAME
+
 echo "Applying Kubernetes Service..."
 kubectl apply -f k8s/service.yaml
+
+echo "Waiting for rollout to finish..."
+kubectl rollout status deployment/$DEPLOYMENT_NAME
 
 echo "Getting PODS"
 kubectl get pods
 
 echo "Getting services"
 kubectl get services
+
+echo "Fetching the main service"
+kubectl get services $SERVICE_NAME
